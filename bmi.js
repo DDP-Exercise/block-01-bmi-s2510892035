058c4ca2f3cfcbebd577a515c854ec36cecf3556
s2510892035 <s2510892035@students.fh-hagenberg.at>
2026-03-09 01:37:02 +0100

    Add files via upload
    Abgabe

--- bmi.js
+++ bmi.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 /*******************************************************
  *     bmi.js
  *
@@ -28,8 +28,8 @@
  * Make sure, to help your users understand what they need to type in, by using clear prompt-instructions.
  */
 
-const LINE = "-----------------------------------------------------";
-let bmr, bmi, normal, danger;
+// const LINE = "-----------------------------------------------------";
+// let bmr, bmi, normal, danger;
 
 /**
  * Formulas:
@@ -71,9 +71,82 @@ let bmr, bmi, normal, danger;
  *   -----------------------------------------------------
  */
 
-console.log(LINE); // Logs the dashed-line.
+//console.log(LINE); // Logs the dashed-line.
 
 /*
  * TODO: Make sure to TEST YOUR SOFTWARE! Does it work, when People are smaller than 1 meter? Or taller than 2?
  * Tip: An 18-Year old Woman, sized 160cm with 60 kg should have a BMR of 1467 kcal and a BMI of 23.4375.
  */
+
+let surname = prompt("What is your surname?");
+let name = prompt("What is your name?");
+console.log("-----------------------------------------------------")
+console.log("Name:\t\t\t\t  " + surname.toUpperCase() + " " + name);
+console.log("-----------------------------------------------------")
+
+let age = Number(prompt("What is your age?"));
+if (isNaN(age)) {
+    console.log("Please enter a valid age!");
+}
+else {
+    console.log("Age:\t\t\t\t  " + age + " Years");
+}
+
+let heightCm = Number(prompt("What is your height in cm?"));
+if (isNaN(heightCm)) {
+    console.log("Please enter a valid height!");
+}
+else {
+    let heightM = heightCm / 100; // Conversion to meters
+    console.log("Height:\t\t\t\t  " + heightM.toFixed(2) + " m");
+}
+
+let weight = Number(prompt("What is your weight?"));
+if (isNaN(weight)) {
+    console.log("Please enter a valid weight!");
+}
+else {
+    console.log("Weight:\t\t\t\t  " + weight + " kg");
+}
+
+//calculation
+let method = prompt("Please type your gender: female or male").toLowerCase();
+if (method === "female") {
+     A = 655;
+     B = 10;
+     C = 2;
+     D = 6;
+     BMR = (A + B * weight + C * heightCm - D * age);
+     console.log("Basal Metabolic Rate: " + BMR + " kcal")
+
+}
+else if (method === "male") {
+     A = 66;
+     B = 14;
+     C = 5;
+     D = 7;
+     BMR = (A + B * weight + C * heightCm - D * age);
+     console.log("Basal Metabolic Rate: " + BMR + " kcal")
+}
+
+let BMI = (10000 * weight) / Math.pow(heightCm, 2);
+if (isNaN(BMI)) {
+    console.log("No calculation because wrong input!");
+}
+else {
+    console.log("BMI:\t\t\t\t  " + BMI);
+}
+
+let normal = "NO";
+let danger = "NO";
+
+if (BMI >= 18.5 && BMI <= 24.9) {
+    normal = "YES";
+}
+if (BMI < 16 || BMI > 30) {
+    danger ="YES"
+}
+
+console.log("NormalWeight:\t\t  " + normal);
+console.log("Danger:\t\t\t\t  " + danger);
+console.log("-----------------------------------------------------")
